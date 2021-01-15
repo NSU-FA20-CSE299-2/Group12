a5225c7ce5439802c75ab33657e04ad44b27eba7
Usama Hossain <mohammed.usama@northsouth.edu>
2021-01-16 02:15:27 +0600

    Enhancement #9: added registration and profile view

---  Code/fitfoodie/users/views.py
+++  Code/fitfoodie/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, CreateView
+from django.urls import reverse_lazy
+
+from .forms import ProfileCreationForm
+from .models import Profile
 
 # Create your views here.
+
+
+class ProfileView(DetailView):
+    model = Profile
+    template_name = 'profile.html'
+
+
+class RegistrationView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    template_name = 'registration/registration.html'
+    success_url = reverse_lazy('register_success')

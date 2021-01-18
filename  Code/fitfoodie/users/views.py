d239f45723b9495efb9bc6376ee1ab5477eb435f
Usama Hossain <scarredentos@gmail.com>
2021-01-18 15:38:02 +0600

    Enhancement #16: added views for homepage

---  Code/fitfoodie/users/views.py
+++  Code/fitfoodie/users/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, TemplateView
 from django.urls import reverse_lazy
 
 from .forms import ProfileCreationForm
 from .models import Profile
 
 # Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
 
 class ProfileView(DetailView):
@@ -18,3 +22,5 @@ class RegistrationView(CreateView):
     form_class = ProfileCreationForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('register_success')
+
+
